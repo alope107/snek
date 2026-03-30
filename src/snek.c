40ee9f135d989f1c1d89c0c89d7f93e900e26f61
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-30 12:20:34 -0700

    Deleted food logic and switched to direction enum

--- src/snek.c
+++ src/snek.c
@@ -8,36 +8,18 @@ const color food_color = MAKE_COLOR(0, 0, 31);
 inline int col_val(int loc) {return loc & 0xff;}
 inline int row_val(int loc) {return (loc & 0xff00) >> 8;}
 
-inline unsigned int prng(unsigned int *seed, unsigned int max) {
-    *seed ^= *seed << 13;
-    *seed ^= *seed >> 17;
-    *seed ^= *seed << 5;
-    return *seed % max;
-}
+typedef enum {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+} direction;
 
-void place_food(unsigned int *seed) {
-    int placed = 0;
-
-    while(!placed) {
-        int test_row = prng(seed, SCREEN_HEIGHT);
-        int test_col = prng(seed, SCREEN_WIDTH);
-
-        if (VRAM[test_row][test_col] == 0) {
-            placed = 1;
-            VRAM[test_row][test_col] = food_color;
-        }
-    }
-}
 
 int main() {
     REG_DISPCNT = DCNT_BG2 | DCNT_MODE3;
 
-    // Randomly chosen from a Broadway song
-    unsigned int seed = 525600;
-
     unsigned int snek_len = 10;
-
-    place_food(&seed);
 
     int directions[4][2] = {
         {-1, 0},
@@ -47,7 +29,7 @@ int main() {
     };
 
     int row=50, col=50;
-    int direction = 0;
+    direction dir = UP;
 
 
 
@@ -57,23 +39,18 @@ int main() {
 
     while(1) {
         vblank();
-        if(KEY_HELD(KEY_UP) && direction != 2) {
-            direction = 0;
-        } else if (KEY_HELD(KEY_RIGHT) && direction != 3) {
-            direction = 1;
-        } else if (KEY_HELD(KEY_DOWN) && direction != 0) {
-            direction = 2;
-        } else if (KEY_HELD(KEY_LEFT) && direction != 1) {
-            direction = 3;
+        if(KEY_HELD(KEY_UP) && dir != DOWN) {
+            dir = UP;
+        } else if (KEY_HELD(KEY_RIGHT) && dir != LEFT) {
+            dir = RIGHT;
+        } else if (KEY_HELD(KEY_DOWN) && dir != UP) {
+            dir = DOWN;
+        } else if (KEY_HELD(KEY_LEFT) && dir != RIGHT) {
+            dir = LEFT;
         }
 
-        
-        if(KEY_HELD(KEY_B)) {
-            place_food(&seed);
-        }
-
-        row = WRAP(row+directions[direction][0], SCREEN_HEIGHT);
-        col = WRAP(col+directions[direction][1], SCREEN_WIDTH);
+        row = WRAP(row+directions[dir][0], SCREEN_HEIGHT);
+        col = WRAP(col+directions[dir][1], SCREEN_WIDTH);
 
         if(VRAM[row][col] == food_color && snek_len < MAX_SNEK_LEN) {
             snek_len++;
