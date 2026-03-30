d56b74cc69c747946701d21a9f87b632f65161a1
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-30 12:31:37 -0700

    location typedef and named directions array

--- src/snek.c
+++ src/snek.c
@@ -3,10 +3,13 @@
 #define MAX_SNEK_LEN 100
 
 const color snek_color = MAKE_COLOR(31, 0, 0);
-const color food_color = MAKE_COLOR(0, 0, 31);
 
-inline int col_val(int loc) {return loc & 0xff;}
-inline int row_val(int loc) {return (loc & 0xff00) >> 8;}
+typedef unsigned int location;
+
+inline location make_loc(int row, int col) { return (row << 8) | col; }
+inline unsigned int col_val(location loc) {return loc & 0xff;}
+inline unsigned int row_val(location loc) {return (loc & 0xff00) >> 8;}
+
 
 typedef enum {
     UP,
@@ -15,25 +18,23 @@ typedef enum {
     LEFT
 } direction;
 
+const int directions[4][2] = {
+    [UP]    = {-1, 0}, 
+    [RIGHT] = {0, 1}, 
+    [DOWN]  = {1, 0}, 
+    [LEFT]  = {0, -1}, 
+};
+
 
 int main() {
     REG_DISPCNT = DCNT_BG2 | DCNT_MODE3;
 
     unsigned int snek_len = 10;
 
-    int directions[4][2] = {
-        {-1, 0},
-        {0, 1},
-        {1, 0},
-        {0, -1},
-    };
-
     int row=50, col=50;
     direction dir = UP;
 
-
-
-    int history[MAX_SNEK_LEN] = {};
+    location history[MAX_SNEK_LEN] = {};
     int hist_head = 0;
     int hist_tail = 0;
 
@@ -51,18 +52,13 @@ int main() {
 
         row = WRAP(row+directions[dir][0], SCREEN_HEIGHT);
         col = WRAP(col+directions[dir][1], SCREEN_WIDTH);
-
-        if(VRAM[row][col] == food_color && snek_len < MAX_SNEK_LEN) {
-            snek_len++;
-        } 
         
         VRAM[row][col] = snek_color;
 
-        history[hist_head] = (row << 8) | col;
+        history[hist_head] = make_loc(row, col);
         hist_head++;
 
         int wrapped_tail = hist_tail < hist_head ? hist_tail : hist_tail - MAX_SNEK_LEN;
-
         if(hist_head - wrapped_tail == snek_len) {
             VRAM[row_val(history[hist_tail])][col_val(history[hist_tail])] = 0;
             hist_tail++;
